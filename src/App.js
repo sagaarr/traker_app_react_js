c5b93195f8644457bf3d490cfc0b0ea94a4bb754
sagar <sagar@carcility.com>
2021-12-16 11:52:40 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import Login from "./pages/Login/Login";
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,66 +1,116 @@
 import React from "react";
-import { Dropdown, DropdownButton, Uncontrolle } from "react-bootstrap";
-import { LinkContainer } from 'react-router-bootstrap';
+import {
+  Accordion,
+  Button,
+  Dropdown,
+  DropdownButton,
+  Offcanvas,
+  Uncontrolle,
+} from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, withRouter } from "react-router-dom";
 import { logoutAction } from "../utils/redux/actions/loginAction";
 import { openRoute, protectedRoutes } from "../navigation/paths";
+import { useState } from "react";
+import "./Header.css";
 
 const Header = (props) => {
-
   const dispatch = useDispatch();
   const { isLoggedIn } = useSelector((state) => state.loginReducer);
-
   const logoutHandler = () => {
-    dispatch(logoutAction())
-    props.history.push(openRoute.signIn)
-  }
+    dispatch(logoutAction());
+    props.history.push(openRoute.signIn);
+  };
+
+  const [show, setShow] = useState(false);
+  const handleShow = () => {
+    setShow((s) => !s);
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light fixed-top">
       <div className="container">
+        <Button variant="primary" onClick={handleShow}>
+          Launch
+        </Button>
         <Link className="navbar-brand" to={"/users"}>
           UnM
         </Link>
         <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
           <ul className="navbar-nav ml-auto">
-            {isLoggedIn ? (
-              <>
-              
-                <DropdownButton id="dropdown-basic-button" title="Ambulance" className="droopdown-btn">
-                <LinkContainer to={protectedRoutes.getAllAmbulances}><Dropdown.Item >Ambulances</Dropdown.Item></LinkContainer>
-                <LinkContainer to={protectedRoutes.createAmbulance}><Dropdown.Item >Create Ambulance</Dropdown.Item></LinkContainer>
-                <LinkContainer to={protectedRoutes.ambulanceCrew}><Dropdown.Item >Create Ambulance Crew</Dropdown.Item></LinkContainer>
-                </DropdownButton>
-                
-                <DropdownButton id="dropdown-basic-button" title="Crew" className="droopdown-btn">
-                <LinkContainer to="/create_user"><Dropdown.Item >Create Driver or EMT</Dropdown.Item></LinkContainer>
-                <LinkContainer to="/sub-link1"><Dropdown.Item >Attendance</Dropdown.Item></LinkContainer>
-                </DropdownButton>
-                <DropdownButton id="dropdown-basic-button" title="Reports" className="droopdown-btn">
-                <LinkContainer to={protectedRoutes.dailyReport}><Dropdown.Item >Daily Report</Dropdown.Item></LinkContainer>
-                <LinkContainer to={protectedRoutes.patentReport}><Dropdown.Item >Patent Report</Dropdown.Item></LinkContainer>
-                </DropdownButton>
-                <li className="nav-item">
-                  <a
-                    style={{ cursor: "pointer" }}
-                    className="nav-link"
-                    onClick={logoutHandler}
-                  >
-                    Logout
-                  </a>
-                </li>
-              
-              </>
-            ) : (
-              <>
-                <li className="nav-item">
-                  <Link className="nav-link" to={"/sign-in"}>
-                    Login
-                  </Link>
-                </li>
-                
-              </>
-            )}
+            <Offcanvas show={show} onHide={handleShow}>
+              <Offcanvas.Header closeButton>
+                <Offcanvas.Title>Offcanvas</Offcanvas.Title>
+              </Offcanvas.Header>
+              <Offcanvas.Body>
+                {isLoggedIn ? (
+                  <>
+                    <Link to="/users" className="nav-link dashboard-link">
+                      Dashboard
+                    </Link>
+                    <Accordion className="mb-2">
+                      <Accordion.Item eventKey="1">
+                        <Accordion.Header>Ambulance</Accordion.Header>
+                        <Accordion.Body>
+                          <LinkContainer to={protectedRoutes.getAllAmbulances}>
+                            <Dropdown.Item>Ambulances</Dropdown.Item>
+                          </LinkContainer>
+                          <LinkContainer to={protectedRoutes.createAmbulance}>
+                            <Dropdown.Item>Create Ambulance</Dropdown.Item>
+                          </LinkContainer>
+                          <LinkContainer to={protectedRoutes.ambulanceCrew}>
+                            <Dropdown.Item>Create Ambulance Crew</Dropdown.Item>
+                          </LinkContainer>
+                        </Accordion.Body>
+                      </Accordion.Item>
+                    </Accordion>
+
+                    <Accordion className="mb-2">
+                      <Accordion.Item eventKey="1">
+                        <Accordion.Header>Pilot OR EMT</Accordion.Header>
+                        <Accordion.Body>
+                          <LinkContainer to="/create_user">
+                            <Dropdown.Item>Create Driver or EMT</Dropdown.Item>
+                          </LinkContainer>
+                          <LinkContainer to="/sub-link1">
+                            <Dropdown.Item>Attendance</Dropdown.Item>
+                          </LinkContainer>
+                        </Accordion.Body>
+                      </Accordion.Item>
+                    </Accordion>
+
+                    <Accordion className="mb-2">
+                      <Accordion.Item eventKey="1">
+                        <Accordion.Header>Reports</Accordion.Header>
+                        <Accordion.Body>
+                          <LinkContainer to={protectedRoutes.dailyReport}>
+                            <Dropdown.Item>Daily Report</Dropdown.Item>
+                          </LinkContainer>
+                          <LinkContainer to={protectedRoutes.patentReport}>
+                            <Dropdown.Item>Patent Report</Dropdown.Item>
+                          </LinkContainer>
+                        </Accordion.Body>
+                      </Accordion.Item>
+                    </Accordion>
+                    <button
+                      className="nav-link logout-btn"
+                      onClick={logoutHandler}
+                    >
+                      Logout
+                    </button>
+                  </>
+                ) : (
+                  <>
+                    <li className="nav-item">
+                      <Link className="nav-link" to={"/sign-in"}>
+                        Login
+                      </Link>
+                    </li>
+                  </>
+                )}
+              </Offcanvas.Body>
+            </Offcanvas>
           </ul>
         </div>
       </div>
